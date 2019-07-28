a0e2142755c4d554226c692b1c22bbf3a5cf75d6
Artuoso <2424759611@qq.com>
2019-07-28 22:54:01 +0800

    modify: 单元测试类修改

--- src/test/java/com/uni/DemoTest.java
+++ src/test/java/com/uni/DemoTest.java
@@ -22,37 +22,73 @@ public class DemoTest {
     @Autowired
     private DemoUserService demoUserService;
 
+    /**
+     *
+     *
+     * @author Created by zc on 2019/7/28
+     */
     @Test
-    public void testDemo() {
+    public void logTest() {
+        logger.trace("----这是trace");
+        logger.debug("----这是debug");
+        logger.info("----这是info");
+        logger.warn("--这是warn");
+        logger.error("----这是error");
+    }
+
+    /**
+     * 插入数据测试
+     *
+     * @author Created by zc on 2019/7/28
+     */
+    @Test
+    public void inserTest() {
         DemoUser user = new DemoUser();
         user.setName("玄彩娥");
         user.setHaveSectFlag(DemoUser.HAVE_SECT_FLAG_YES);
         user.setSectName("普陀山");
         user.setRoundCount(36);
 
-//        demoUserService.insert(user);
-//        logger.debug("主键ID是：" + user.getId());
-
-        DemoUser selectUser = demoUserService.get(5L);
-        logger.debug("id：" + selectUser.getId() + " " + selectUser.getName() + " " + selectUser.getSectName());
-//
-//        selectUser.setSectName("方寸山");
-//        demoUserService.update(selectUser);
-
-//        DemoUser upUser = new DemoUser();
-//        upUser.setId(2L);
-//        upUser.setRoundCount(20);
-//        demoUserService.updatePartially(upUser);
-
+        demoUserService.insert(user);
+        logger.debug("主键ID是：" + user.getId());
     }
 
+    /**
+     * 查询数据测试
+     *
+     * @author Created by zc on 2019/7/28
+     */
     @Test
-    public void testLog() {
-        logger.trace("----这是trace");
-        logger.debug("----这是debug");
-        logger.info("----这是info");
-        logger.warn("--这是warn");
-        logger.error("----这是error");
+    public void selectTest() {
+        DemoUser selectUser = demoUserService.get(5L);
+        logger.debug("id：" + selectUser.getId() + " " + selectUser.getName() + " " + selectUser.getSectName());
+    }
+
+    /**
+     * 更新数据测试
+     *
+     * @author Created by zc on 2019/7/28
+     */
+    @Test
+    public void updateTest() {
+        DemoUser selectUser = demoUserService.get(5L);
+        selectUser.setSectName("方寸山");
+        demoUserService.update(selectUser);
+
+        DemoUser upUser = new DemoUser();
+        upUser.setId(2L);
+        upUser.setRoundCount(20);
+        demoUserService.updatePartially(upUser);
+    }
+
+    /**
+     * 删除数据测试
+     *
+     * @author Created by zc on 2019/7/28
+     */
+    @Test
+    public void deleteTest() {
+        demoUserService.delete(6L);
     }
 
     /**
